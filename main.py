0abeb9325dacac752befc0cfa50da70f806e8727
paritosh <tripathiparitosh935@gmail.com>
2021-09-28 19:55:41 +0530

    added the youtube scrapping code

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ time_published = []
 
 def main():
     driver = webdriver.Chrome('/home/paritosh/PycharmProjects/Youtube/chromedriver')
-    driver.get('https://www.youtube.com/results?search_query=python+tutorial')
+    driver.get('https://www.youtube.com/results?search_query=python+programming+tutorial')
     content = driver.page_source.encode('utf-8').strip()
     soup = BeautifulSoup(content, 'lxml')
     titles = soup.findAll('a', id='video-title')
@@ -24,7 +24,7 @@ def main():
     j = 0
     for title in titles[1:]:
         name.append(title.text)
-        view.append(views[i + 1].text)
+        view.append(views[i+1].text)
         time_published.append(views[i].text)
         base = "https://www.youtube.com/watch?v="
         link = video_urls[j].get('href')
