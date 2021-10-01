117dcd60988a44dc7281b064dbc315e2b5824d95
paritosh <tripathiparitosh935@gmail.com>
2021-10-01 19:47:33 +0530

    update

--- Youtube_courses/python.py
+++ Youtube_courses/python.py
@@ -14,7 +14,17 @@ time_published = []
 
 def main():
     driver = webdriver.Chrome('/home/paritosh/PycharmProjects/Youtube/chromedriver')
-    driver.get('https://www.youtube.com/results?search_query=python+programming+tutorial')
+    driver.get('https://www.youtube.com/results?search_query=python+programming+tutorial+in+english+&sp=CAMSAhAB')
+
+    while True:
+        scroll_height = 2000
+        document_height_before = driver.execute_script("return document.documentElement.scrollHeight")
+        driver.execute_script(f"window.scrollTo(0, {document_height_before+ scroll_height});")
+        time.sleep(1.5)
+        document_height_after = driver.execute_script("return document.documentElement.scrollHeight")
+        if document_height_after == document_height_before:
+            break
+
     content = driver.page_source.encode('utf-8').strip()
     soup = BeautifulSoup(content, 'lxml')
     titles = soup.findAll('a', id='video-title')
@@ -22,10 +32,10 @@ def main():
     video_urls = soup.findAll('a', id='video-title')
     i = 0
     j = 0
-    for title in titles[1:]:
+    for title in titles[0:]:
         name.append(title.text)
-        view.append(views[i+1].text)
-        time_published.append(views[i].text)
+        view.append(views[i+2].text)
+        time_published.append(views[i+1].text)
         base = "https://www.youtube.com/watch?v="
         link = video_urls[j].get('href')
         url = urljoin(base, link)
