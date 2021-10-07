439d786bc8cded698a5398f873a4b1d5a40682f3
Paritosh tripathi <tripathiparitosh935@gmail.com>
2021-10-07 17:34:46 +0530

    update

--- Youtube_courses/python.py
+++ Youtube_courses/python.py
@@ -11,42 +11,45 @@ name = []
 view = []
 time_published = []
 
+programming_languages = ["python", "java", "Cpp"]
+
+driver = webdriver.Chrome('/home/paritosh/PycharmProjects/Youtube/chromedriver')
+"""
+        while True:
+            scroll_height = 3000
+            document_height_before = driver.execute_script("return document.documentElement.scrollHeight")
+            driver.execute_script(f"window.scrollTo(0, {document_height_before + scroll_height});")
+            time.sleep(1.5)
+            document_height_after = driver.execute_script("return document.documentElement.scrollHeight")
+            if document_height_after == document_height_before:
+                break    
+"""
 
 def main():
-    driver = webdriver.Chrome('/home/paritosh/PycharmProjects/Youtube/chromedriver')
-    driver.get('https://www.youtube.com/results?search_query=python+programming+tutorial+in+english+&sp=CAMSAhAB')
+    for i in range(0, len(programming_languages)):
+        driver.get('https://www.youtube.com/results?search_query={}+programming+tutorial+&sp=CAMSAhAB'.format(programming_languages[i]))
 
-    while True:
-        scroll_height = 2000
-        document_height_before = driver.execute_script("return document.documentElement.scrollHeight")
-        driver.execute_script(f"window.scrollTo(0, {document_height_before+ scroll_height});")
-        time.sleep(1.5)
-        document_height_after = driver.execute_script("return document.documentElement.scrollHeight")
-        if document_height_after == document_height_before:
-            break
+        content = driver.page_source.encode('utf-8').strip()
+        soup = BeautifulSoup(content, 'lxml')
+        titles = soup.findAll('a', id='video-title')
+        video_urls = soup.findAll('a', id='video-title')
+        i = 0
+        j = 0
 
-    content = driver.page_source.encode('utf-8').strip()
-    soup = BeautifulSoup(content, 'lxml')
-    titles = soup.findAll('a', id='video-title')
-    views = soup.findAll('span', class_="style-scope ytd-video-meta-block")
-    video_urls = soup.findAll('a', id='video-title')
-    i = 0
-    j = 0
-    for title in titles[0:]:
-        name.append(title.text)
-        view.append(views[i+2].text)
-        time_published.append(views[i+1].text)
-        base = "https://www.youtube.com/watch?v="
-        link = video_urls[j].get('href')
-        url = urljoin(base, link)
-        urls.append(url)
-        i += 2
-        j += 1
+        for title in titles[0:]:
+            name.append(title.text)
+            base = "https://www.youtube.com/watch?v="
+            link = video_urls[j].get('href')
+            url = urljoin(base, link)
+            urls.append(url)
+            i += 2
+            j += 1
 
 
 main()
 
-df = pd.DataFrame({"links": urls, "names": name, "views": view, "age": time_published})
+
+df = pd.DataFrame({"links": urls, "names": name})
 df.to_csv("youtube_python.csv")
 
 end = time.time()
