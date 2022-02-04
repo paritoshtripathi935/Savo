dda533d2de019b0a6a7921651732dfb92c2b6ca9
Paritosh Tripathi <tripathiparitosh935@gmail.com>
2022-02-04 19:55:04 +0530

    added login page

--- Savoweb/Savoweb/urls.py
+++ Savoweb/Savoweb/urls.py
@@ -16,8 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
-from Savoweb import accounts
-from course import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- Savoweb/accounts/views.py
+++ Savoweb/accounts/views.py
@@ -2,6 +2,8 @@ import re
 from django.shortcuts import render
 
 # Create your views here.
+# This is the views files user login
+
 
 def register(request):
     return render(request, 'sign-up.html')
