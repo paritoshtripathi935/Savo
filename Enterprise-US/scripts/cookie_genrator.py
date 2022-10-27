543a7b9fdf452f35e71c826f5f60ae6bd4dce60e
Ubuntu <ubuntu@ip-172-31-47-33.ap-south-1.compute.internal>
2022-10-27 09:43:28 +0000

    Enterprise US

---
+++ Enterprise-US/scripts/cookie_genrator.py
@@ -0,0 +1,58 @@
+# Cookies for enterprise-us
+# _cs_mk=0.05535408080938886_1666090539074; 
+# ak_bmsc=2EB6774AFBE045AD7A4EADFA5080F80E~000000000000000000000000000000~YAAQlLxWaJ4LT+aDAQAAzi686hHbtJnuhC6AK7NKzzkqB18yZGa2Usq/6S1KYe1fiZS+kOdHG6EI1tZIdWwg1rqrJV5kdMNs/cdPV/VkjT+29LdI1os08NveG7gjBQXbyxE5eOwwTDFI6zEcrLW7TzZqVE3gZsgdqlsb7LqZ7+686M3XlEhiSOMKRSr8FDhiv/DMQWdZgZtvlSnhfvTzQEIDjiRrTxkpGFqpxXDn8OxueLt4FmWoE0BbnqmDQtMAYQoT7FopAKh0M/uhlSJLPIrwwXtwgqji5BYx2bwgre32qLe0lDkFLc/xzP7RALDq6LZtHKrbBVv0b6/HH3zwUcELMO2uGNNjFu7jUwdymNyJ0jQCs5B+tvkXne0h24obqz2SFi/Z7vm9P+NCpv2XFO/u8s0eNRfwS/5TdvFBbm9kMJz8j3WpWjpR3/ZGExKhJPbvNfwOCBESB1yHkSQFUKeMusynTdsUTwpoZl/Zztr79DDn4o3AzNADNC8CBFE=; 
+# at_check=true; 
+# s_ecid=MCMID%7C40283505539795236569037949592543695545; 
+# AMCVS_30545A0C536B768C0A490D44%40AdobeOrg=1; 
+# s_cc=true; 
+# visid_incap_1271375=a9eb4ZKqRfqNXWmmIb3aLoyGTmMAAAAAQUIPAAAAAABtT2/iWvJjq/YvUlp3q9c3; 
+# SESSION=NWZhODFlNmMtMGJlZC00ZTJiLTk1OTUtMjIwOTdhMTMxYjEx; 
+# nlbi_1271375=asCsAsFm4ntxxXkm9V/w9gAAAADpwz5KRjtJrHQC6Vu7GJ91; 
+# incap_ses_9218_1271375=W6XpdWpMGGgUmlW0uersf46GTmMAAAAAf4vfLkrm0emoq5SuuzKWpg==; 
+# bm_sv=07DADDF22E2D4BCA6A4C9BC4138F7784~YAAQpbxWaKFHY+eDAQAAIlDU6hHL/LQhDN4EiAEJ6Q+fSTolpPIOw0Mn89WbKhcdyF8mYjn9+pjsnFjhVVRjECsvMNC8m9S4mSTWS3G+y87hJlWPqWPu1s8aRv8fECPPIayfISXpo5/6RwWSS+xGmKq4PRlKUJn6IvAUeLZ7fNkM9CX1pATeqSYuFtwRdbnvdB+6FaH0oBSgK5FsafuO9I1oytVsG4dvGS7GCTmORnqUD6cLXxgXPH0Muz2xx/XldVOzXQ==~1; 
+# s_tps=12; 
+# s_pvs=16; 
+# OptanonConsent=isIABGlobal=false&datestamp=Tue+Oct+18+2022+16%3A52%3A16+GMT%2B0530+(India+Standard+Time)&version=6.39.0&consentId=332710a2-60ea-427f-b978-fb7192d469cb&interactionCount=1&landingPath=NotLandingPage&groups=C0001%3A1%2CC0003%3A1%2CC0004%3A1&hosts=H1%3A1%2CH31%3A1%2CH324%3A1%2CH32%3A1%2CH48%3A1%2CH43%3A1%2CH14%3A1%2CH233%3A1%2CH19%3A1%2CH10%3A1%2CH2%3A1%2CH24%3A1%2CH25%3A1%2CH26%3A1%2CH46%3A1%2CH5%3A1%2CH229%3A1%2CH297%3A1%2CH27%3A1%2CH8%3A1%2CH299%3A1%2CH230%3A1%2CH28%3A1%2CH47%3A1%2CH300%3A1%2CH13%3A1%2CH38%3A1%2CH29%3A1%2CH15%3A1%2CH16%3A1%2CH17%3A1&genVendors=V11%3A1%2CV4%3A1%2CV33%3A1%2CV60%3A0%2CV22%3A1%2CV5%3A1%2CV14%3A1%2CV37%3A1%2CV41%3A1%2CV43%3A1%2CV3%3A1%2CV35%3A1%2CV25%3A1%2CV21%3A1%2CV20%3A1%2CV59%3A1%2CV40%3A1%2CV16%3A1%2CV26%3A1%2CV34%3A1%2CV9%3A1%2CV27%3A1%2CV39%3A1%2CV31%3A1%2CV24%3A1%2CV46%3A1%2CV45%3A1%2CV36%3A1%2CV58%3A1%2CV55%3A1%2CV28%3A1%2CV10%3A1%2CV50%3A1%2CV49%3A1%2CV13%3A1%2CV17%3A1%2CV7%3A1%2CV52%3A1%2CV30%3A1%2CV47%3A1%2CV48%3A1%2CV12%3A1%2CV18%3A1%2CV19%3A1%2CV15%3A1%2CV29%3A1%2CV56%3A1%2CV42%3A1%2CV38%3A1%2CV51%3A1%2CV8%3A1%2CV32%3A1%2CV57%3A1%2CV44%3A1%2CV53%3A1%2CV54%3A1%2CV23%3A1%2CV6%3A1%2C&AwaitingReconsent=false&geolocation=IN%3BBR; OptanonAlertBoxClosed=2022-10-18T11:22:16.205Z; mbox=session#57fec984f6d94ec29037ff4e20dd46bd#1666093999|PC#57fec984f6d94ec29037ff4e20dd46bd.31_0#1729336939; s_pers=%20s_vs%3D1%7C1666093938959%3B%20gpv_v5%3D%252Fen%252Freserve.html%7C1666093938989%3B%20s_visit%3D1%7C1666093938995%3B; s_sess=%20SC_LINKS%3D%3B%20s_ppvl%3D%252Fen%252Fhome.html%252C18%252C18%252C845%252C1366%252C626%252C1366%252C768%252C1%252CL%3B%20s_ppv%3D%252Fen%252Freserve.html%252C39%252C0%252C0%252C1366%252C626%252C1366%252C768%252C1%252CL%3B%20s_ptc%3D%3B; 
+# s_sq=%5B%5BB%5D%5D; 
+# AMCV_30545A0C536B768C0A490D44%40AdobeOrg=1176715910%7CMCIDTS%7C19284%7CMCMID%7C40283505539795236569037949592543695545%7CMCAID%7CNONE%7CMCOPTOUT-1666099339s%7CNONE%7CvVersion%7C5.4.0
+
+# cookies generated till now 
+#incap_ses_712_1271375=cZtiWPr9umnsdMmS0ZzhCeIUWmMAAAAAvL3tV8gytSw9wloyMZSRtw== 
+#nlbi_1271375=eiqzf4f0WSe6dp7a9V/w9gAAAACUOWyEYNrDJmeZsr+SrJDR
+#visid_incap_1271375=uVa/TjnHRO+DhrCervHxluIUWmMAAAAAQUIPAAAAAAA8NJU/69N1wTW1xw9jh+jV
+#SESSION=YTA5NWFlY2MtNTBkYS00ZjA3LTk3ZjQtY2RiYzc0ZDI3Nzcw
+
+import requests
+
+
+response = requests.get('https://prd-west.webapi.enterprise.com/enterprise-ewt/enterprise/reservations/vehicles/availability')
+api = 'https://prd-west.webapi.enterprise.com/enterprise-ewt/enterprise/reservations/vehicles/availability'
+cookie = response.cookies
+cookie_dict = requests.utils.dict_from_cookiejar(cookie)
+cookie_str = '; '.join([str(x) + '=' + str(y) for x, y in cookie_dict.items()])
+
+headers = {
+    "authority": "prd-west.webapi.enterprise.com",
+    "method": "GET",
+    "path": "/enterprise-ewt/enterprise/reservations/vehicles/availability",
+    "scheme": "https",
+    "accept": "application/json, text/plain, */*",
+    "accept-encoding": "gzip, deflate, br",
+    "accept-language": "en-US,en;q=0.8",
+    "brand": "enterprise",
+    "channel": "web",
+    "cookie": cookie_str,
+    "domain_country_of_residence": "US",
+    "origin": "https://www.enterprise.com",
+    "referer": "https://www.enterprise.com/",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-site",
+    "sec-gpc": "1",
+    "tab_id": "1666090538837_1",
+    "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Safari/537.36"
+}
+
+# pass the cookie to the next request
+response = requests.get(api, headers=headers)
+print(response.text)
---
+++ Enterprise-US/scripts/main.py
@@ -0,0 +1,382 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+from requests.adapters import HTTPAdapter
+from requests.packages.urllib3.util.retry import Retry
+from requests import get
+import pandas as pd
+from concurrency import Concurrency
+import random
+import time
+import os
+import gzip
+import datetime
+from concurrent.futures import ThreadPoolExecutor
+from concurrent.futures import as_completed
+from datetime import timedelta
+import sys
+
+#start = time.time()
+ip_list = []
+
+request = requests.Session()
+retry = Retry(connect=5, backoff_factor=0.1)
+adapter = HTTPAdapter(max_retries=retry)
+request.mount('http://', adapter)
+request.mount('https://', adapter)
+
+Datetime = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+timestamp = datetime.datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+date = datetime.date.today()
+
+class Scraper:
+    def __init__(self):
+        self.headers = json.load(open('Getaround-UK/Data/headers/headers.json'))
+        self.get_car_api = "https://uk.getaround.com/search.json?address={}&address_source=google&administrative_area=undefined&car_sharing=false&city_display_name={}&country_scope=GB&display_view=list&end_date={}&end_time={}&latitude={}&longitude={}&only_responsive=true&page=1&picked_car_ids=EMPTY&poi_id=EMPTY&start_date={}&start_time={}&user_interacted_with_car_sharing=false&view_mode=list"
+        self.get_fare_api = "https://uk.getaround.com/request_availability?car_id={}&end_date={}&end_time={}&start_date={}&start_time={}&web_version=true"                      
+        self.servicable_api  = "https://uk.getaround.com/autocomplete/address?delivery_points_of_interest_only=false&enable_google_places=true&enable_imprecise_addresses=true&input={}"     
+        self.category_data_frames = []
+        self.fare_list = []
+        self.servicable_list = []
+        self.storage_path = "/mnt/efs/fs1/raw/enterprise/us/"
+
+
+    def join_get_car_api(self, address, city, end_date, end_time, start_date, start_time, latitude, longitude):
+        return self.get_car_api.format(address, city, end_date, end_time, latitude, longitude, start_date, start_time)
+    
+
+    def join_get_fare_api(self, car_id, end_date, end_time, start_date, start_time):
+        return self.get_fare_api.format(car_id, end_date, end_time, start_date, start_time)
+
+
+    def ServiceableArea(self, address, ip):
+        try:
+            api = self.servicable_api.format(address)
+
+            proxies = {
+                'http': f'http://{ip}:3128',
+                'https': f'http://{ip}:3128'
+            }
+            response = request.get(api, proxies=proxies)
+            print('response.status_code: ', response.status_code, "of", address, "at", ip, "of ServiceableArea")
+            if response.status_code == 200:
+                self.servicable_list.append(response.json())
+            return response.status_code
+
+        except Exception as e:
+            print('error: ', e)
+
+    
+    def GetCarData(self, address, city, end_date, end_time, start_date, start_time, latitude, longitude, ip):
+        try:
+            api = self.join_get_car_api(address, city, end_date, end_time, start_date, start_time, latitude, longitude)
+
+            proxies = {
+                'http': f'http://{ip}:3128',
+                'https': f'http://{ip}:3128'
+            }
+
+            retry_counter_cardata = 0
+            retry_counter_faredata = 0
+            retry_counter_servicablearea = 0
+            
+            if self.ServiceableArea(address, ip) == 200:
+                    response = request.get(api, proxies=proxies, headers=self.headers)
+                    print("ServiceableArea: ", address, response.status_code)
+
+                    while response.status_code != 200:
+                        retry_counter_cardata += 1
+
+                        response = request.get(api, proxies=proxies, headers=self.headers)
+                        print("Retrying Car Api", address, response.status_code, ip)
+
+                        time.sleep(random.randint(1, 5))
+                        if response.status_code == 200:
+
+                            if (response.status_code == 200):
+                                parsed = response.json()
+                                print("Successfull Response", response.status_code, "for", address, "of", "GetCarData")
+                                self.category_data_frames.append(parsed)
+
+                                if parsed['cars'] == []:
+                                    print("No Cars Found")
+                                    return
+
+                                for car in parsed['cars']:
+                                    car_id = car['id']
+                                    self.GetFareData(car_id, end_date, end_time, start_date, start_time, ip)
+                            break
+                            
+                        if retry_counter_cardata == 5:
+                            break
+
+                    if (response.status_code == 200):
+                        parsed = response.json()
+                        print("Successfull Response", response.status_code, "for", address, "of", "GetCarData")
+                        self.category_data_frames.append(parsed)
+                        
+                        if parsed['cars'] == []:
+                            print("No Cars Found")
+                            return
+
+                        for car in parsed['cars']:
+                            car_id = car['id']
+                            self.GetFareData(car_id, end_date, end_time, start_date, start_time, ip)
+                            print("Successfull Response", response.status_code, "for", address, "of", "GetFareData")
+                    else:
+                        print("Failed Response", response.status_code, "for", address, "of", "GetCarData")
+                        return
+                        
+            else:
+                print("{}is not serviceable".format(address),"on ip", ip)
+
+            """
+            while self.ServiceableArea(address, ip) != 200 and self.ServiceableArea(address, ip) == 429:
+                retry_counter_servicablearea += 1
+                print("Retrying Service Api: ", retry_counter_servicablearea, "of", ip)
+
+                time.sleep(random.randint(1, 5))
+                
+                if self.ServiceableArea(address, ip) == 200:
+                    response = request.get(api, proxies=proxies)
+                    print("ServiceableArea: ", address, response.status_code)
+
+                    while response.status_code != 200:
+                        retry_counter_cardata += 1
+
+                        response = request.get(api, proxies=proxies)
+                        print("Retrying Car Api", address, response.status_code, ip)
+
+                        time.sleep(random.randint(1, 5))
+                        if response.status_code == 200:
+
+                            if (response.status_code == 200):
+                                parsed = response.json()
+                                print("Successfull Response", response.status_code, "for", address, "of", "GetCarData")
+                                self.category_data_frames.append(parsed)
+
+                                if parsed['cars'] == []:
+                                    return
+
+                                for car in parsed['cars']:
+                                    car_id = car['id']
+                                    self.GetFareData(car_id, end_date, end_time, start_date, start_time, ip)
+                            break
+                            
+                        if retry_counter_cardata == 5:
+                            break
+
+                    if (response.status_code == 200):
+                        parsed = response.json()
+                        print("Successfull Response", response.status_code, "for", address, "of", "GetCarData")
+                        self.category_data_frames.append(parsed)
+                        if parsed['cars'] == []:
+                            print("No Cars Found", address, "of", "GetCarData")
+                            return
+
+                        for car in parsed['cars']:
+                            car_id = car['id']
+                            self.GetFareData(car_id, end_date, end_time, start_date, start_time, ip)
+                        
+                else:
+                    print("{}is not serviceable".format(address),"on ip", ip)
+                
+                if retry_counter_servicablearea == 5:
+                    break
+            """
+        except Exception as e:
+        #   print line number and error
+            print('error: ', e)
+            print('line number: ', sys.exc_info()[-1].tb_lineno)
+            pass
+    
+
+    def GetFareData(self, car_id, end_date, end_time, start_date, start_time, ip):
+        try:
+            api = self.join_get_fare_api(car_id, end_date, end_time, start_date, start_time)
+
+            proxies = {
+                'http': f'http://{ip}:3128',
+                'https': f'http://{ip}:3128'
+            }
+
+            response = request.get(api, proxies=proxies)
+
+            if (response.status_code == 200):
+                parsed = response.json()
+                self.fare_list.append(parsed)   
+
+                print('Sucessful Response: ', 'response.status_code: ', response.status_code, "of", car_id, "at", ip, "of FareData")
+                return response.status_code
+            else:
+                print('Unsuccessful Response: ', 'response.status_code: ', response.status_code, "of", car_id, "at", ip, "of FareData")
+                return response.status_code
+
+        except Exception as e:
+            print('error: ', e)
+
+
+    
+    def CarDataWriter(self, latitude, longitude):
+        try:
+
+            if not os.path.exists(self.storage_path + "raw/"):
+                os.makedirs(self.storage_path + "raw/")
+            
+            if not os.path.exists(self.storage_path + "raw/" + Datetime + "/" + "Location/" + str(latitude) + "," + str(longitude) + "/"):
+                os.makedirs(self.storage_path + "raw/" + Datetime + "/" + "Location/" + str(latitude) + "," + str(longitude) + "/")
+            
+            with gzip.open(self.storage_path + "raw/" + Datetime + "/" + "Location/" + str(latitude) + "," + str(longitude) + "/" + timestamp + ".json.gz", 'wt') as f:
+                json.dump(self.category_data_frames, f)
+                # clear the data after writing
+                self.category_data_frames.clear()
+                
+                print("CarDataWriter: is written in raw folder", latitude, longitude)
+
+        except Exception as e:
+            print('Data Write Error of CarDataWriter: ', e)
+    
+
+    def FareDataWriter(self, filename):
+        try:
+            df = pd.DataFrame(self.fare_list)
+            df.to_json(filename, orient='records', lines=True)
+        except Exception as e:
+            print('Data Write Error of FareDataWriter: ', e)
+
+    
+    def ServiceableAreaDataWriter(self, filename):
+        try:
+            df = pd.DataFrame(self.servicable_list)
+            df.to_json(filename, orient='records', lines=True)
+        except Exception as e:
+            print('Data Write Error of ServiceableAreaDataWriter: ', e)
+
+    def FullCarDataWriter(self, filename):
+        try:
+            df = pd.DataFrame(self.category_data_frames)
+            df.to_json(filename, orient='records', lines=True)
+        except Exception as e:
+            print('Data Write Error of FullCarDataWriter: ', e)
+
+
+    def create_ec2_instance(self, number_of_instance):
+        print("Creating {} EC2 Proxy Instance".format(number_of_instance))
+        url = "http://172.31.39.71:8000/create/"
+
+        payload = {
+                    "template_name":"SquidProxyServer",
+                    "template_version":"3",
+                    "rival":"getaround-uk",
+                    "ownerARN":"arn:aws:iam::047719688069:user/paritosh.tripathi@anakin.company",
+                    "region": "ap-south-1",
+                    "count":number_of_instance
+        }
+        response = requests.request("POST", url, json=payload)
+
+        for key, value in response.json().items():
+            ip_list.append(value['private'])
+        
+        print("All EC2 Proxy Instance created")
+
+
+    def DeleteInstance(self):
+        print("Deleting EC2 Proxy instance")
+        url = "http://172.31.39.71:8000/terminate/"
+
+        payload = {
+                    "template_name":"SquidProxyServer",
+                    "template_version":"3",
+                    "rival":"getaround-uk",
+                    "ownerARN":"arn:aws:iam::047719688069:user/paritosh.tripathi@anakin.company",
+                    "region": "ap-south-1"
+            }
+        response = requests.request("POST", url, json=payload)
+
+        print("All Instances are deleted")
+    
+    def ipRotation(self,n):
+        print("IP Rotation")
+        self.DeleteInstance()
+        ip_list.clear()
+        self.create_ec2_instance(n)
+        print("IP Rotation Completed")
+
+    def date_generator(self):
+        start_date = date.today()
+        # start date is tommorow
+        start_date = start_date + timedelta(days=1)
+        start_date_gap = [ 0, 1, 2, 3, 4, 5, 6, 7, 14, 21, 28, 56, 84]
+        end_date_gap =  [1, 2, 3, 4, 5, 6, 7, 14, 28]
+        date_list = []
+        for x in start_date_gap:
+            new_start_date = start_date + timedelta(days=x)
+            for y in end_date_gap:
+                new_end_date = new_start_date + timedelta(days=y)
+                date_list.append([new_start_date, new_end_date])
+        return date_list
+
+if __name__ == "__main__":
+    try:
+        scraper = Scraper()
+        df = pd.read_json('Getaround-UK/Data/geojson/google_geoJson.json', orient='records', lines=True) 
+        n = 5
+        scraper.create_ec2_instance(n)
+        time.sleep(5)
+        print("Waiting for EC2 instances to start")
+        data_chunck = []
+        try:
+            date_list = scraper.date_generator()
+
+            for i in range(0, 100):
+                for start_date, end_date in date_list:
+                    address = df[0][i]['address']
+                    city = df[0][i]['address'].split(",")[0].strip()
+                    end_date = end_date.strftime("%Y-%m-%d")
+                    end_time = "07%3A00"
+                    start_date = start_date.strftime("%Y-%m-%d")
+                    start_time = "06%3A00"
+                    latitude = df[0][i]['lat']
+                    longitude = df[0][i]['lon']
+                    ip = ip_list[i%n]
+                    data_chunck.append([address, city, end_date, end_time, start_date, start_time, latitude, longitude,ip])
+                
+            print(len(data_chunck))
+            print(data_chunck[1])
+
+            concc = Concurrency(worker_thread=5)
+            concc.run_thread(func=scraper.GetCarData, 
+            param_list=data_chunck, param_name=["address", "city", "end_date", "end_time", "start_date", "start_time", "latitude", "longitude", "ip"])
+            
+            #scraper.CarDataWriter(latitude, longitude)
+            scraper.FullCarDataWriter('{}test_9_car.json'.format(scraper.storage_path))
+            scraper.FareDataWriter('{}test_9_fare.json'.format(scraper.storage_path))
+            scraper.ServiceableAreaDataWriter('{}test_9_serviceable.json'.format(scraper.storage_path))
+
+        except Exception as e:
+            print(e)
+            pass
+        
+        scraper.DeleteInstance()
+
+    except KeyboardInterrupt:
+        print('KeyboardInterrupt')
+        scraper.DeleteInstance()
+        scraper.FullCarDataWriter('{}test_9_car.json'.format(scraper.storage_path))
+        scraper.FareDataWriter('{}test_9_fare.json'.format(scraper.storage_path))
+        scraper.ServiceableAreaDataWriter('{}test_9_serviceable.json'.format(scraper.storage_path))
+        print('Data Saved')
+        pass    
+
+
+"""
+end = time.time()
+
+with open('Data/time.txt', 'a+') as f:
+    Datetime = datetime.datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+    f.write(str(end - start))
+    f.write(" " + Datetime)
+    # add new line
+    f.write("\n")
+
+"""
